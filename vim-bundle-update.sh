3e2c5cd4c36b8d22ac1e3a7ded74960c98e37c66
Von Welch <von@vwelch.com>
2022-03-06 16:39:21 -0500

    vim-bundle-update.sh: Update for nvim, call UpdateRemotePlugins

--- vim-bundle-update.sh
+++ vim-bundle-update.sh
@@ -4,20 +4,27 @@
 #  Kudos: https://github.com/junegunn/vim-plug/issues/730
 #  Uses autoload/plug.vim
 
+# For vim:
 # "-T dumb" lets me see all the output
 # "-E" Improved Ex mode, stops complaint about output not being
 #     to a TTY.
 # Call "set nomore" turns off waiting for user during output
-vim='vim -T dumb -E'
+# vim='vim -T dumb -E'
 
-# XXX I'm not sure what this actually does. May need tweaking
-#     to commit upgrade to git.
+# For neovim
+# "-Es" Silent Ex mode - do not start a UI.
+vim='nvim -Es'
+
 echo "Updating vim-plug"
-${vim} -c "set nomore|PlugUpgrade|qall" &> /dev/null
+${vim} -c "set nomore|PlugUpgrade|qall"
 
 echo "Updating vim-plug bundles"
 # Need 'qall' here instead of just 'quit'
-${vim} -c "set nomore|PlugUpdate!|qall" &> /dev/null
+${vim} -c "set nomore|PlugUpdate!|qall"
+
+# This is needed with some frequency so that deoplete completes utilisnips
+echo "Running UpdateRemotePlugins"
+${vim} -c "set nomore|UpdateRemotePlugins|qall"
 
 # Not calling 'PlugClean' - do that manually.
 
